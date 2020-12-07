12bc704ec736d5e83432d5030223c20d388b1bf4
hoangpham95 <hoanghpham95@gmail.com>
2020-12-07 12:55:31 -0800

    Upgrade RN to 0.63.4 for Image displaying

--- legacy/client/app/ios/app/AppDelegate.m
+++ legacy/client/app/ios/app/AppDelegate.m
@@ -8,7 +8,7 @@
 #import <RNSplashScreen.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED 
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -31,7 +31,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+#if FB_SONARKIT_ENABLED 
   InitializeFlipper(application);
 #endif
   [[FBSDKApplicationDelegate sharedInstance] application:application
