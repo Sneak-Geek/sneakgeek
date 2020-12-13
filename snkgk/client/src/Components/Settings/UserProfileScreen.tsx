e05774dc0b98374bc0221ff7b9b4d966179a743c
vcoz17 <ntduc97@gmail.com>
2020-12-12 19:38:21 -0800

    dm anh Hoang

---
+++ snkgk/client/src/Components/Settings/UserProfileScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, SafeAreaView } from 'react-native';
+
+class UserProfileScreen extends React.Component {
+
+
+    render() {
+        return (
+            <View> </View>
+        );
+    }
+
+}
+export default UserProfileScreen;
