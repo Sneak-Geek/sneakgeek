d8e7ab0d1b63e4cf4baa6aa98f3f3863a8aae5fe
hoangpham95 <hoanghpham95@gmail.com>
2021-04-26 17:07:37 -0700

    Admin controller for order.

--- legacy/service/src/infra/dao/OrderDao/IOrderDao.ts
+++ legacy/service/src/infra/dao/OrderDao/IOrderDao.ts
@@ -27,4 +27,6 @@ export interface IOrderDao {
   destroyById(OrderId: string): Promise<Order>;
   getLastSold(top: number): Promise<TrendingOrder[]>;
   getOrderHistoryByUserId(buyerId: string): Promise<OrderHistory[]>;
+  getAllPendingOrders(start: number, pageSize: number): Promise<OrderHistory[]>;
+  getOrderById(orderId: string): Promise<OrderHistory>;
 }
