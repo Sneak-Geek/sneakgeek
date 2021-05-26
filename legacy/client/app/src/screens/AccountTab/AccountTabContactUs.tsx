a8060bd9b3d1bfd7d6f9fabfabbe5a022fd0e92f
hoangpham95 <hoanghpham95@gmail.com>
2021-05-26 13:53:04 -0400

    SNKGK-126 fix empty contact page.

--- legacy/client/app/src/screens/AccountTab/AccountTabContactUs.tsx
+++ legacy/client/app/src/screens/AccountTab/AccountTabContactUs.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { ListItem } from 'react-native-elements';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { strings, themes } from 'resources';
+import {ListItem} from 'react-native-elements';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {strings, themes} from 'resources';
 
 export const ContactUs: React.FC<{}> = () => {
   return (
     <SafeAreaView style={{flex: 1, paddingTop: 0}}>
-      <ListItem title={strings.CustomerSupportPOCName} subtitle={strings.CustomerSupportEmail} titleStyle={themes.TextStyle.body} subtitleStyle={themes.TextStyle.callout}/>
+      <ListItem>
+        <ListItem.Content>
+          <ListItem.Title style={themes.TextStyle.body}>
+            {strings.CustomerSupportPOCName}
+          </ListItem.Title>
+          <ListItem.Subtitle
+            style={[themes.TextStyle.footnoteRegular, {marginTop: 8}]}>
+            Email: {strings.CustomerSupportEmail}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
     </SafeAreaView>
-  )
-}
+  );
+};
