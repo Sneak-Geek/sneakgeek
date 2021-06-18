Sneak-Geek/sneakgeek

commit e9efb194efbb7b8d885a37bbc139bec693d29f7f

SNKGK-199 Add thousand separator to sell price (#132)

client/app/src/screens/Order/NewSellOrder/NewSellOrder.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {TextInput, View} from 'react-native';
+import {View} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {themes, strings} from 'resources';
 import {
@@ -20,6 +20,8 @@ import {
 } from 'actions';
 import {IInventoryService} from 'business/src';
 import {IAppState} from 'store/AppStore';
+import {TextInputMask} from 'react-native-masked-text';
+
 
 type Props = {
   userProfile: Profile;
@@ -38,6 +40,7 @@ type SellDetailChild = {
 
 type State = {
   inventory: Partial<Inventory>;
+  numText: Array<string>;
 };
 
 @connect(
@@ -71,7 +74,41 @@ export class NewSellOrder extends React.Component<Props, State> {
         shoeSize: undefined,
         quantity: undefined,
       },
+      numText: ['', '', '']
     };
+  }
+
+  onUpdate = (text, index) => {
+    if (index == 0)
+    {
+      this.setState({
+        inventory: {
+          ...this.state.inventory,
+          shoeSize: text,
+        },
+        numText: [text, this.state.numText[1], this.state.numText[2]],
+      })
+    }
+    if (index == 1)
+    {
+      this.setState({
+        inventory: {
+          ...this.state.inventory,
+          quantity: parseInt(text, 10),
+        },
+        numText: [this.state.numText[0], text, this.state.numText[2]],
+      })
+    }
+    if (index == 2)
+    {
+      this.setState({
+        inventory: {
+          ...this.state.inventory,
+          sellPrice: parseInt(text, 10),
+        },
+        numText: [this.state.numText[0], this.state.numText[1], text],
+      })
+    }
   }
 
   public render(): JSX.Element {
@@ -87,47 +124,25 @@ export class NewSellOrder extends React.Component<Props, State> {
   }
 
   private _renderInventory() {
+
     const inventoryItems = [
       {
         title: strings.ShoeSize,
         displayText: '',
-        onUpdate: (text) => {
-          this.setState({
-            inventory: {
-              ...this.state.inventory,
-              shoeSize: text,
-            },
-          });
-        },
       },
       {
         title: strings.InventoryQuantity,
         displayText: '',
-        onUpdate: (text) => {
-          this.setState({
-            inventory: {
-              ...this.state.inventory,
-              quantity: parseInt(text, 10),
-            },
-          });
-        },
       },
       {
         title: strings.Price,
         displayText: '',
-        onUpdate: (text) => {
-          this.setState({
-            inventory: {
-              ...this.state.inventory,
-              sellPrice: parseInt(text, 10),
-            },
-          });
-        },
       },
     ];
+
     return (
       <View style={{padding: 20, flex: 1, flexDirection: 'column'}}>
-        {inventoryItems.map((t) => (
+        {inventoryItems.map((t, index) => (
           <View
             style={{
               flexDirection: 'row',
@@ -135,14 +150,29 @@ export class NewSellOrder extends React.Component<Props, State> {
               justifyContent: 'space-between',
             }}>
             <AppText.SubHeadline>{t.title}</AppText.SubHeadline>
-            <TextInput
+            <TextInputMask
               placeholderTextColor={themes.AppDisabledColor}
               placeholder={t.title}
               numberOfLines={1}
+              keyboardType={'number-pad'}
+              type={index == 2 ? 'money' : 'custom'}
+              options={
+                index == 2 ? 
+                {
+                  precision: 0,
+                  separator: '.',
+                  delimiter: '.',
+                  unit: '',
+                  suffixUnit: 'đ'
+                } : 
+                {
+                  mask: ''
+                } 
+              }
+              value={this.state.numText[index]}
+              onChangeText={text => this.onUpdate(text, index)}
               style={{...themes.TextStyle.body, marginBottom: 20}}
-              keyboardType={'numeric'}
-              onChangeText={t.onUpdate}
-            />
+              />
           </View>
         ))}
       </View>
@@ -199,3 +229,4 @@ export class NewSellOrder extends React.Component<Props, State> {
     }
   }
 }
+

commit b5ae9440eb802357582c3a907434ba896f6a3c84

Fix input bugs caused by commit e9efb194

client/app/src/screens/Order/NewSellOrder/NewSellOrder.tsx
@@ -61,6 +61,7 @@ type State = {
 )
 export class NewSellOrder extends React.Component<Props, State> {
   private _shoe: Shoe;
+  private MoneyField: TextInputMask
 
   public constructor(props: Props) {
     super(props);
@@ -110,7 +111,7 @@ export class NewSellOrder extends React.Component<Props, State> {
       this.setState({
         inventory: {
           ...this.state.inventory,
-          sellPrice: parseInt(text, 10),
+          sellPrice: parseInt(this.MoneyField.getRawValue(), 10) * 10,
         },
         numText: [this.state.numText[0], this.state.numText[1], text],
       })
@@ -161,23 +162,24 @@ export class NewSellOrder extends React.Component<Props, State> {
               placeholder={t.title}
               numberOfLines={1}
               keyboardType={'number-pad'}
-              type={index == 2 ? 'money' : 'custom'}
+              type={index === 2 ? 'money' : 'custom'}
               options={
-                index == 2 ? 
+                index === 2 ? 
                 {
                   precision: 0,
                   separator: '.',
-                  delimiter: '.',
+                  delimiter: ',',
                   unit: '',
                   suffixUnit: 'đ'
                 } : 
                 {
-                  mask: ''
+                  mask: '****'
                 } 
               }
               value={this.state.numText[index]}
               onChangeText={text => this.onUpdate(text, index)}
               style={{...themes.TextStyle.body, marginBottom: 20}}
+              ref={(ref) => {index === 2 ? this.MoneyField = ref : undefined}}
               />
           </View>
         ))}
@@ -227,6 +229,7 @@ export class NewSellOrder extends React.Component<Props, State> {
       this.props.showSuccessNotification('Đã bán thành công sản phẩm!');
       this.props.navigation.goBack();
     } catch (error) {
+      console.log(error);
       this.props.showErrorNotification(
         'Đã có lỗi xảy ra, xin vui lòng thử lại',
       );