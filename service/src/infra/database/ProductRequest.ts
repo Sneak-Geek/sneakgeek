edb292dfa147e351e9383fc017d51e2a4899effb
Hoang Pham <hoanghpham95@gmail.com>
2021-07-04 01:45:46 -0700

    Fix size.

--- service/src/infra/database/ProductRequest.ts
+++ service/src/infra/database/ProductRequest.ts
@@ -29,7 +29,7 @@ export const ProductRequestSchema = new mongoose.Schema(
     },
     size: {
       type: String,
-      enum: ShoeSize.Adult,
+      enum: [...ShoeSize.Adult, ShoeSize.GradeSchool, ShoeSize.PreSchool, ShoeSize.Toddler],
     },
     requestStatus: {
       type: String,
