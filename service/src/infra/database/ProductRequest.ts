4e4a23c0912680f793220ea149b318c876157e6b
hoangpham95 <hoanghpham95@gmail.com>
2021-07-04 02:21:42 -0700

    Lint

--- service/src/infra/database/ProductRequest.ts
+++ service/src/infra/database/ProductRequest.ts
@@ -29,7 +29,12 @@ export const ProductRequestSchema = new mongoose.Schema(
     },
     size: {
       type: String,
-      enum: [...ShoeSize.Adult, ...ShoeSize.GradeSchool, ...ShoeSize.PreSchool, ...ShoeSize.Toddler],
+      enum: [
+        ...ShoeSize.Adult,
+        ...ShoeSize.GradeSchool,
+        ...ShoeSize.PreSchool,
+        ...ShoeSize.Toddler,
+      ],
     },
     requestStatus: {
       type: String,
