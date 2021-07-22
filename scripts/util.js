9dbda8cd01e4dcf2e8a8ef3f2887762a77e6309a
Hoang Pham <hoanghpham95@gmail.com>
2021-07-22 09:33:22 -0700

    SNKGK-233 Add release script for Prod (#158)

---
+++ scripts/util.js
@@ -0,0 +1,30 @@
+const { execFile } = require("child_process");
+const fs = require("fs");
+
+function execute(command) {
+  const script = "./util_execute";
+  fs.writeFileSync(script, command);
+  fs.chmodSync(script, 0755);
+
+  execFile(script,
+    {
+      env: {
+        PATH: process.env.PATH
+      }
+    },
+    (error, stdout, stderr) => {
+      try {
+        fs.unlinkSync(script);
+      } catch (error) {
+
+      }
+      if (error) {
+        console.error(stderr);
+        console.error(error);
+        throw error;
+      }
+      console.log(stdout);
+    });
+}
+
+module.exports = { execute };
