e32d00784c71014bd1230263093f5496b03dd355
hoangpham95 <huyhoang.pham95@gmail.com>
2021-07-22 09:20:43 -0700

    Add tag and versioning.

--- scripts/bump_version.js
+++ scripts/bump_version.js
@@ -13,43 +13,43 @@ const today = `${(date.getMonth() + 1).toString().padStart(2, '0')}${date.getDat
 const commonVersion = `${releaseBaseVersion}.${today}`;
 
 function increateVersionAndTag() {
-    const curVer = version[type].version.split(".")[2];
-    if (today === curVer) {
-        version[type].build += 1;
-    } else {
-        version[type].build = 1;
-    }
-    type === "app" ? version[type].version = commonVersion : version[type].version = today;
-    const gitClientTag = `${commonVersion}-${version.app.build}-${type}`;
-    execute(`git tag ${gitClientTag}`);
+  const curVer = version[type].version.split(".")[2];
+  if (today === curVer) {
+    version[type].build += 1;
+  } else {
+    version[type].build = 1;
+  }
+  type === "app" ? version[type].version = commonVersion : version[type].version = today;
+  const gitClientTag = `${commonVersion}-${version.app.build}-${type}`;
+  execute(`git tag ${gitClientTag}`);
 }
 
 function main() {
-    increateVersionAndTag();
-    switch (type) {
-        case "app":
-            execute(`
+  increateVersionAndTag();
+  switch (type) {
+    case "app":
+      execute(`
                 cd ${process.cwd()}/client/app
                 export IOS_VERSION_NUMBER=${version.app.version}
                 export IOS_BUILD_NUMBER=${version.app.build}
                 fastlane ios bump_version_number
                 fastlane ios bump_build_number
             `);
-            break;
-        case "service":
-            break;
-        default:
-            console.error("Invalid \"type\" flag, exit");
-            process.exit(1);
-            break;
+      break;
+    case "service":
+      break;
+    default:
+      console.error("Invalid \"type\" flag, exit");
+      process.exit(1);
+      break;
+  }
+  const versionString = JSON.stringify(version, null, 2);
+  fs.writeFile(`${process.cwd()}/version.json`, versionString, (err) => {
+    if (err) {
+      process.exit(1);
+      console.error(err);
     }
-    const versionString = JSON.stringify(version, null, 2);
-    fs.writeFile(`${process.cwd()}/version.json`, versionString, (err) => {
-        if (err) {
-            process.exit(1);
-            console.error(err);
-        }
-    })
+  })
 }
 
 main();
--- scripts/util.js
+++ scripts/util.js
@@ -2,29 +2,29 @@ const { execFile } = require("child_process");
 const fs = require("fs");
 
 function execute(command) {
-    const script = "./util_execute";
-    fs.writeFileSync(script, command);
-    fs.chmodSync(script, 0755);
+  const script = "./util_execute";
+  fs.writeFileSync(script, command);
+  fs.chmodSync(script, 0755);
 
-    execFile(script,
-        {
-            env: {
-                PATH: process.env.PATH
-            }
-        },
-        (error, stdout, stderr) => {
-            try {
-                fs.unlinkSync(script);
-            } catch (error) {
+  execFile(script,
+    {
+      env: {
+        PATH: process.env.PATH
+      }
+    },
+    (error, stdout, stderr) => {
+      try {
+        fs.unlinkSync(script);
+      } catch (error) {
 
-            }
-            if (error) {
-                console.error(stderr);
-                console.error(error);
-                throw error;
-            }
-            console.log(stdout);
-        });
+      }
+      if (error) {
+        console.error(stderr);
+        console.error(error);
+        throw error;
+      }
+      console.log(stdout);
+    });
 }
 
 module.exports = { execute };
