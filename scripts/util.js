1d3aee5f7635e41d50d491d216ee1414bfd81a7f
hoangpham95 <huyhoang.pham95@gmail.com>
2021-07-22 18:51:02 -0700

    Add auto release staging.

--- scripts/util.js
+++ scripts/util.js
@@ -2,29 +2,31 @@ const { execFile } = require("child_process");
 const fs = require("fs");
 
 function execute(command) {
-  const script = "./util_execute";
-  fs.writeFileSync(script, command);
-  fs.chmodSync(script, 0755);
+  return new Promise((resolve, reject) => {
+    const script = "./util_execute";
+    fs.writeFileSync(script, command);
+    fs.chmodSync(script, 0755);
 
-  execFile(script,
-    {
-      env: {
-        PATH: process.env.PATH
-      }
-    },
-    (error, stdout, stderr) => {
-      try {
-        fs.unlinkSync(script);
-      } catch (error) {
-
-      }
-      if (error) {
-        console.error(stderr);
-        console.error(error);
-        throw error;
-      }
-      console.log(stdout);
-    });
+    execFile(script,
+      {
+        env: {
+          PATH: process.env.PATH
+        }
+      },
+      (error, stdout, stderr) => {
+        try {
+          fs.unlinkSync(script);
+        } catch (error) { }
+        if (error) {
+          console.error(stderr);
+          console.error(error);
+          reject(error);
+        } else {
+          console.log(stdout);
+          resolve();
+        }
+      });
+  })
 }
 
 module.exports = { execute };
