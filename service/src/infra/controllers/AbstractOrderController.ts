098a9f51a5f39b2159cae1217556e9bf35edd561
Thinh Ly <thinhgly@gmail.com>
2021-07-25 12:06:02 -0400

    Update notification email for seller, user and admin

--- service/src/infra/controllers/AbstractOrderController.ts
+++ service/src/infra/controllers/AbstractOrderController.ts
@@ -31,33 +31,124 @@ export abstract class AsbtractOrderController {
 
     // TO DO: Ask anh Dai to come up with the messages sending to buyers/sellers/admin
     let buyerText = "";
-    const subject = `Your SneakGeek Order Status Update`;
-    const adminText = `Order number ${orderId} status has been changed to ${status}`;
+    var buyerSubject = "";
+    var adminSubject = "";
+    var adminText = `Order number ${orderId} status has been changed to ${status}`;
     let sellerText = `Order number ${orderId} status has been changed to ${status}`;
+    var sellerSubject = "";
 
+    if (status === TrackingStatus.WAITING_FOR_BANK_TRANSFER){
+      buyerSubject = "[SneakGeek] Đơn hàng khởi tạo thành công";
+      buyerText = `Đơn hàng của bạn đã được khởi tạo thành công:
+                    Mã đơn hàng: ${orderId}
+                    Sản phẩm: ${inventory.shoeInfo.name}
+                    Size: ${inventory.shoeSize}
+                    Đơn giá: ${inventory.sellPrice}
+                    Quý khách vui lòng chuyển khoản theo thông tin trên ứng dụng trong vòng 12h để hoàn tất đơn hàng.
+                    
+                    SneakGeek Team`
+      adminSubject = `Đơn hàng ${orderId} được khởi tạo`
+      adminText = `Đơn hàng mới ${orderId} đc khởi tạo, check xem đã nhận được tiền trong tài khoản ngân hàng chưa?
+                   Tên người mua: ${buyer.userProvidedName}
+                   Thông tin sản phẩm: 
+                      Tên giày: ${inventory.shoeInfo.name}
+                      Size: ${inventory.shoeSize}
+                   Số tiền: ${inventory.sellPrice}`
+    }
     if (status === TrackingStatus.RECEIVED_BANK_TRANSFER) {
-      buyerText = `Your order has been placed. The confirmation code is ${orderId}`;
-    } else if (
-      status === TrackingStatus.NOT_RECEIVED_BANK_TRANSFER ||
+      buyerSubject = "[SneakGeek] Đơn hàng được hoàn tất";
+      buyerText = `Đơn hàng ${orderId} của bạn đã được hoàn tất. 
+                   Sản phẩm sẽ được SneakGeek chuyển đến cho bạn trong thời gian sớm nhất.
+                   Cảm ơn bạn đã mua hàng trên SneakGeek.
+                   
+                   SneakGeek Team`;
+      sellerSubject = "[SneakGeek] Thông báo đơn hàng mới";
+      sellerText = `Mã đơn hàng: ${orderId}
+                    Sản phẩm: ${inventory.shoeInfo.name}
+                    Size: ${inventory.shoeSize}
+                    Đơn giá: ${inventory.sellPrice}
+                    Cửa hàng vui lòng xác nhận và chuyển đơn hàng đến địa chỉ: ${buyer.userProvidedAddress}
+                    Trân trọng cảm ơn!
+                    
+                    SneakGeek team`;
+    } 
+    else if (status === TrackingStatus.NOT_RECEIVED_BANK_TRANSFER)
+    {
+      buyerSubject = "[SneakGeek] Đơn hàng chưa được hoàn tất";
+      buyerText = `Đơn hàng ${orderId} của bạn đã chưa được hoàn tất do thanh toán chưa được xác nhận thành công trong 12h. 
+                   Đơn hàng sẽ được hủy.
+                   Quý khách vui lòng liên hệ qua email: support@sneakgeek.io để nhận hỗ trợ. 
+                   Xin cảm ơn!
+                    
+                   SneakGeek Team`
+    }
+    else if (status === TrackingStatus.SELLER_APPROVED_ORDER)
+    {
+      var deadline = new Date();
+      deadline.setHours(deadline.getHours()+ 48);
+      adminSubject = `Seller đã approve đơn hàng ${orderId}`;
+      adminText = `Seller đã approved đơn hàng. Seller sẽ chuyển hàng cho SneakGeek trong vòng 48h. 
+                   Đơn hàng: ${orderId}
+                   Thông tin sản phẩm: 
+                    Tên giày: ${inventory.shoeInfo.name}
+                    Size: ${inventory.shoeSize}
+                   Hạn nhận hàng: ${deadline.getDay()}/${deadline.getMonth()}/${deadline.getFullYear()}`
+    }
+    else if(
       status === TrackingStatus.SELLER_REJECTED_ORDER ||
       status === TrackingStatus.SHOE_UNQUALIFIED
     ) {
-      buyerText = `Your order has been cancelled due to ....`;
+      buyerSubject = "[SneakGeek] Đơn hàng bị từ chối";
+      buyerText = `Thông báo đơn hàng bị từ chối.
+                   Người bán đã từ chối đơn hàng ${orderId}
+                   Đơn hàng đã bị hủy. 
+                   Quý khách lưu ý cập nhật thông tin đơn hàng liên tục trên nền tảng để tránh trường hợp hủy đơn hàng xảy ra trong tương lai. 
+                   Việc hủy đơn hàng nếu diễn ra nhiều có thể ảnh hưởng đến mức phí bán hàng quý khách hàng phải chi trả.
+
+                   SneakGeek Team`;
     } else if (status === TrackingStatus.DELIVERING_TO_BUYER) {
-      buyerText = `Your order has been shipped!`;
-      sellerText = `Order #${orderId} is being sent to buyer. The order status is ${status}.`!;
+      //TODO: Mình có tracking number ko?
+      //buyerText = `Your order has been shipped!`;
+      //sellerText = `Order #${orderId} is being sent to buyer. The order status is ${status}.`!;
     } else if (status === TrackingStatus.BUYER_RECEIVED) {
-      buyerText = `Your order has been delivered!`;
-      sellerText = `Buyer has received order #${orderId}`!;
+      sellerSubject = "[SneakGeek] Hàng đã chuyển đến người mua";
+      sellerText = `Mọi thông tin cần hỗ trợ quý khách vui lòng liên hệ qua email: support@sneakgeek.io.
+                   Xin cảm ơn!
+
+                   SneakGeek Team`;
+      buyerSubject = "[SneakGeek] Cảm ơn quý khách đã mua hàng";
+      buyerText = `Cảm ơn quý khách đã mua hàng trên nền tảng SneakGeek. 
+                   Mọi thông tin cần hỗ trợ quý khách vui lòng liên hệ qua email: support@sneakgeek.io.
+                   Xin cảm ơn!
+
+                   SneakGeek Team`;
     }
-
     if (status === TrackingStatus.SHOE_UNQUALIFIED)
-      sellerText = `Order ${orderId} does not meet our condition guidelines. It will be sent back to the sender.`;
+    {
+      sellerSubject = "[SneakGeek] Giày không đạt chất lượng";
+      sellerText = `Hàng đang được gửi trả cho quý khách.
+                    Đơn hàng: ${orderId}
+                    Thông tin sản phẩm: 
+                      Tên giày: ${inventory.shoeInfo.name}
+                      Size: ${inventory.shoeSize}
+                    Mọi thông tin cần hỗ trợ quý khách vui lòng liên hệ qua email: support@sneakgeek.io.
+                    Xin cảm ơn!
 
-    await Promise.all([
-      this.emailService.notifyUser("ntduc97@gmail.com", subject, adminText),
-      this.emailService.notifyUser(buyerEmail, subject, buyerText),
-      this.emailService.notifyUser(sellerEmail, subject, sellerText),
-    ]);
+                    SneakGeek Team`;
+    }
+    const notiEmail = [];
+    if (adminSubject !== "")
+    {
+      notiEmail.push(this.emailService.notifyUser("ntduc97@gmail.com", adminSubject, adminText));
+    }
+    if (buyerSubject !== "")
+    {
+      notiEmail.push(this.emailService.notifyUser(buyerEmail, buyerSubject, buyerText));
+    }
+    if (sellerSubject !== "")
+    {
+      notiEmail.push(this.emailService.notifyUser(sellerEmail, sellerSubject, sellerText));
+    }
+    await Promise.all(notiEmail);
   }
 }
