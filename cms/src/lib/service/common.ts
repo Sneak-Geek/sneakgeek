2b0363403073b424874c75ec68c5d59d135ca669
Hoang Pham <hoanghpham95@gmail.com>
2021-07-31 11:57:15 -0700

    Add environment variable for cms. (#175)

--- cms/src/lib/service/common.ts
+++
@@ -1,4 +0,0 @@
-// TODO(hoangpham95): Add environment variable
-export const SERVICE_ENDPOINT = 'https://prod.sneakgeek.io/api/v1';
-// Uncomment this line to point service endpoint to localhost
-// export const SERVICE_ENDPOINT = 'http://localhost:8080/api/v1';
