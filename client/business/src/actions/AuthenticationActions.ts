0094e3b09ad2d2f7dbc154de47589630461ea4cd
Thinh Ly <thinhgly@gmail.com>
2021-08-06 16:30:08 -0400

    Backup current work

--- client/business/src/actions/AuthenticationActions.ts
+++ client/business/src/actions/AuthenticationActions.ts
@@ -4,7 +4,10 @@ import { ObjectFactory, FactoryKeys } from "../loader/kernel";
 import { IAccountService, ISettingsProvider, IFacebookSDK } from "../loader";
 import { SettingsKey, IGoogleSDK, IAppleAuthSdk } from "../loader/interfaces";
 import { getUserProfile } from "./ProfileActions";
+//import {Account} from '../model';
 import { Dispatch } from "redux";
+import {auth, firebase} from '@react-native-firebase/auth';
+import { appleAuth } from '@invertase/react-native-apple-authentication';
 
 export const AuthenticationActions = {
   UPDATE_AUTHENTICATION_STATE: "UPDATE_AUTHENTICATION_STATE",
@@ -15,9 +18,9 @@ export const updateAuthenticationState = createAction<AuthenticationPayload>(
 );
 
 export const getCurrentUser = () => {
-  const accountService = ObjectFactory.getObjectInstance<IAccountService>(
+  /*const accountService = ObjectFactory.getObjectInstance<IAccountService>(
     FactoryKeys.IAccountService
-  );
+  );*/
   const settings = ObjectFactory.getObjectInstance<ISettingsProvider>(
     FactoryKeys.ISettingsProvider
   );
@@ -26,19 +29,19 @@ export const getCurrentUser = () => {
     dispatch(
       updateAuthenticationState({ state: NetworkRequestState.REQUESTING })
     );
-    const token = settings.getValue(SettingsKey.CurrentAccessToken);
-
+    //const token = settings.getValue(SettingsKey.CurrentAccessToken);
+    
     try {
-      const accountPayload = await accountService.getCurrentUser(token);
-      if (accountPayload) {
+      const userProfile = await firebase.auth().currentUser;
+      if (userProfile) {
         await settings.loadServerSettings();
+        dispatch(getUserProfile());
         dispatch(
           updateAuthenticationState({
             state: NetworkRequestState.SUCCESS,
-            data: accountPayload,
+            data: undefined, //userAccount: Account //userProfile,
           })
         );
-        dispatch(getUserProfile());
       } else {
         dispatch(
           updateAuthenticationState({
@@ -60,9 +63,6 @@ export const authenticateWithEmail = (
   password: string,
   isSignUp: boolean = false
 ) => {
-  const accountService = ObjectFactory.getObjectInstance<IAccountService>(
-    FactoryKeys.IAccountService
-  );
   const settings = ObjectFactory.getObjectInstance<ISettingsProvider>(
     FactoryKeys.ISettingsProvider
   );
@@ -71,33 +71,92 @@ export const authenticateWithEmail = (
     dispatch(
       updateAuthenticationState({ state: NetworkRequestState.REQUESTING })
     );
-    try {
-      const accountPayload = await accountService.emailAuth(
-        email,
-        password,
-        isSignUp
-      );
-      if (accountPayload) {
-        await settings.setValue(
-          SettingsKey.CurrentAccessToken,
-          accountPayload.token
-        );
-        await settings.loadServerSettings();
-
-        await dispatch(getUserProfile());
-        dispatch(
-          updateAuthenticationState({
-            state: NetworkRequestState.SUCCESS,
-            data: accountPayload,
-          })
-        );
+      if (!isSignUp)
+      {
+        try
+        {
+          const response = await firebase.auth().signInWithEmailAndPassword(email, password);
+          if(response.user)
+          {
+            console.log('User signed in!');
+            dispatch(getUserProfile());
+            dispatch(
+              updateAuthenticationState({
+                state: NetworkRequestState.SUCCESS,
+                data: undefined, //userAccount: Account,
+              })
+            );
+          }
+        }
+        catch (error){
+          console.error(error);
+          dispatch(
+            updateAuthenticationState({
+              state: NetworkRequestState.FAILED,
+              error: error,
+            })
+          );
+        }
       }
-    } catch (error) {
-      console.log("Error auth with email", error);
-      dispatch(
-        updateAuthenticationState({ state: NetworkRequestState.FAILED, error })
+      else
+      {
+        try
+        {
+          const response = await firebase.auth().createUserWithEmailAndPassword(email, password)
+          
+          if(response.user)
+          {
+            console.log('User account created & signed in!');
+            dispatch(getUserProfile());
+            dispatch(
+              updateAuthenticationState({
+                state: NetworkRequestState.SUCCESS,
+                data: undefined, //userAccount: Account,
+              })
+            );
+          }
+          /*else
+          {
+            .catch((error) => {
+              if (error.code === 'auth/email-already-in-use') {
+                console.log('That email address is already in use!');
+              }
+
+              if (error.code === 'auth/invalid-email') {
+                console.log('That email address is invalid!');
+              }
+              console.error(error);
+              dispatch(
+                updateAuthenticationState({
+                  state: NetworkRequestState.FAILED,
+                  error: new Error("Empty account"),
+                })
+              );
+            });*/
+        }
+        catch(error){
+          /*if (error.code === 'auth/email-already-in-use') {
+            console.log('That email address is already in use!');
+          }
+
+          if (error.code === 'auth/invalid-email') {
+            console.log('That email address is invalid!');
+          }*/
+          console.error(error);
+          dispatch(
+            updateAuthenticationState({
+              state: NetworkRequestState.FAILED,
+              error: error,
+            })
+          );
+        }
+      }
+      const token = undefined;//await firebase.auth().currentUser.getIdToken();
+      await settings.setValue(
+        SettingsKey.CurrentAccessToken,
+        token
       );
-    }
+      await settings.loadServerSettings();
   };
 };
 
@@ -107,9 +166,9 @@ export const authenticateWithFb = () => {
     const fbSdk = ObjectFactory.getObjectInstance<IFacebookSDK>(
       FactoryKeys.IFacebookSDK
     );
-    const accountService = ObjectFactory.getObjectInstance<IAccountService>(
+    /*const accountService = ObjectFactory.getObjectInstance<IAccountService>(
       FactoryKeys.IAccountService
-    );
+    );*/
     const settings = ObjectFactory.getObjectInstance<ISettingsProvider>(
       FactoryKeys.ISettingsProvider
     );
@@ -125,19 +184,18 @@ export const authenticateWithFb = () => {
         );
       } else {
         const accessToken = await fbSdk.getCurrentAccessToken();
-        const accountPayload = await accountService.login(
-          accessToken,
-          "facebook"
-        );
+        // Create a Firebase credential with the AccessToken
+        const facebookCredential = auth.FacebookAuthProvider.credential(accessToken);
+        auth().signInWithCredential(facebookCredential);
         await settings.setValue(
           SettingsKey.CurrentAccessToken,
-          accountPayload!.token
+          accountPayload!.token? //accessToken
         );
         dispatch(getUserProfile());
         dispatch(
           updateAuthenticationState({
             state: NetworkRequestState.SUCCESS,
-            data: accountPayload,
+            data: undefined, //userAccount: Account,
           })
         );
       }
@@ -190,9 +248,21 @@ export const authenticateWithGoogle = () => {
 
 export const authenticateWithApple = () => {
   return async (dispatch: Dispatch<any>) => {
-    const appleSdk = ObjectFactory.getObjectInstance<IAppleAuthSdk>(
+    /*const appleSdk = ObjectFactory.getObjectInstance<IAppleAuthSdk>(
       FactoryKeys.IAppleAuthSdk
-    );
+    );*/
+
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+    });
+
+    if (!appleAuthRequestResponse.identityToken) {
+      console.error('Apple Sign-In failed - no identify token returned');
+    }
+
+    const { identityToken, nonce } = appleAuthRequestResponse;
+    const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
 
     const accountService = ObjectFactory.getObjectInstance<IAccountService>(
       FactoryKeys.IAccountService
@@ -202,17 +272,18 @@ export const authenticateWithApple = () => {
     );
 
     try {
-      const payload = await appleSdk.signIn();
-      const accountPayload = await accountService.login(`${payload.email}+${payload.idToken}`, "apple");
+      /*const payload = await appleSdk.signIn();
+      const accountPayload = await accountService.login(`${payload.email}+${payload.idToken}`, "apple");*/
+      auth().signInWithCredential(appleCredential);
       await settings.setValue(
         SettingsKey.CurrentAccessToken,
-        accountPayload?.token
+        identityToken //TODO Thinh 08/05/2021: is this the token we want?
       );
       dispatch(getUserProfile());
       dispatch(
         updateAuthenticationState({
           state: NetworkRequestState.SUCCESS,
-          data: accountPayload,
+          data: undefined, //userAccount: Account,
         })
       );
     } catch (error) {
