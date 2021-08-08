bb667ea171527fd694bf61d4149d47111eb19710
vcoz17 <ntduc97@gmail.com>
2021-08-08 13:45:23 -0700

    fix(AccountVerifiedMiddleware): update account verified middleware

--- service/src/infra/middlewares/AccountVerifiedMiddleware.ts
+++ service/src/infra/middlewares/AccountVerifiedMiddleware.ts
@@ -4,22 +4,35 @@
 
 import { NextFunction, Request, Response } from "express";
 import HttpStatus from "http-status";
-import { UserAccount } from "../database";
+import Server from "../../Server"
+import { IFirebaseAuthService } from "../services/FirebaseAuthService";
+import { Types } from "../../configuration/inversify/inversify.types"
+import { LogProvider } from "../providers";
 
 /**
  * This middleware should be used after FirebaseAuthMiddleware
  */
-export const AccountVerifiedMiddleware = (
+export const AccountVerifiedMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const user = req.user as UserAccount;
-  if (!user.isVerified) {
-    return res.status(HttpStatus.FORBIDDEN).send({
-      message: "Forbidden. Account is not verified",
+  const firebase = Server.container.get<IFirebaseAuthService>(Types.FirebaseAuthService);
+  const idToken = req?.headers?.authorization
+  
+  try {
+    const decodedToken = await firebase.verifyIdToken(idToken);
+    if (decodedToken?.email_verified) {
+      return res.status(HttpStatus.FORBIDDEN).send({
+        message: "Forbidden. Account is not verified",
+      });
+    }
+  } catch (error) {
+    LogProvider.instance.error(error);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+      message: "Internal server errror",
     });
   }
-
+  
   return next();
 };
