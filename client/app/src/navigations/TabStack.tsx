b0366465b84e79c69c7ad28cf2300fa8e6f7104f
hoangpham95 <huyhoang.pham95@gmail.com>
2021-08-14 17:18:56 -0700

    Fix search page.

--- client/app/src/navigations/TabStack.tsx
+++ client/app/src/navigations/TabStack.tsx
@@ -22,7 +22,6 @@ import {
   ISettingsProvider,
   FactoryKeys as Keys,
   Profile,
-  Account,
 } from 'business';
 import {NotificationsScreen} from 'screens/HomeTab';
 import {ProductRequest} from 'screens/SearchTab';
@@ -206,10 +205,7 @@ export const TabStack: React.FC<RootTabProps> = () => {
   const profile: Profile = useSelector(
     (state: IAppState) => state?.UserState?.profileState?.profile,
   );
-  const account: Account = useSelector(
-    (state: IAppState) => state?.UserState?.accountState?.account,
-  );
-  const showInventory = Boolean(profile && account) && profile?.isSeller;
+  const showInventory = Boolean(profile) && profile?.isSeller;
 
   return (
     <Tab.Navigator
