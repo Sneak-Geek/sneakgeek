866674dde1d5f8dbf30e23c7b683fc74be33f5ee
hoangpham95 <huyhoang.pham95@gmail.com>
2021-08-15 17:22:04 -0700

    Fix deployment.

--- cms/src/App.tsx
+++ cms/src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
       setIsInitialized(true);
     }
   }, [isInitialized, setIsInitialized]);
-
+  console.log("Environment:", process.env.NODE_ENV);
   return (
     <Admin
       dataProvider={getDataProvider()}
